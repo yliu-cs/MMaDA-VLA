8a55568194dd3279756fe1bc38a2069c802ebd44
yliu-cs <yliu.cs.cn@gmail.com>
2026-03-26 08:44:36 +0000

    chore: update .gitignore and remove ignored files

--- scripts/eval/infer.sh
+++
@@ -1,6 +0,0 @@
-for temperature in 0.1 0.3 0.5 0.8 1.0; do
-    for timesteps in 2 4 6 8 10 12 14 16 18 20 22 24 26 28 30 32; do
-        echo "temperature=${temperature}, timesteps=${timesteps}"
-        python inference.py --temperature ${temperature} --timesteps ${timesteps}
-    done
-done
